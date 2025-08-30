d65c3c685e3bb9b4d5a0384ca3012b4ca0de4a78
Aswin Krishna <aswinkrishna005@gmail.com>
2025-08-30 10:13:53 +0530

    implement coins and making the avatar within the boundary

--- src/main.js
+++ src/main.js
@@ -8,9 +8,9 @@ import { ground, roadTexture, leftWall, rightWall } from "./objects/ground.js";
 import { Robot } from "./objects/robot.js";
 import { Keys, setupKeyboardListeners } from "./input/keyboard.js";
 import { loadAnimatedRobot } from "./objects/animatedRobot.js";
+import { Coin } from "./objects/coins.js";
 
 let robot = new Robot();
-scene.add(light, robot, ground, leftWall, rightWall);
 setupKeyboardListeners();
 
 let mixer;
@@ -20,6 +20,30 @@ loadAnimatedRobot(scene, ({ model, mixer: loadedMixer }) => {
   mixer = loadedMixer;
   robot.velocity = { x: 0, y: 0, z: 0 };
 });
+
+scene.add(light, robot, ground, leftWall, rightWall);
+
+const coins = [];
+const roadWidth = ground.width;
+const coinCount = 20; // spawn 20 coins ahead
+for (let i = 0; i < coinCount; i++) {
+  const x = (Math.random() - 0.5) * (roadWidth - 1); // within road
+  const z = -50 - Math.random() * 700; // ahead of robot
+  const coin = new Coin({ x, y: 0.5, z });
+  coins.push(coin);
+  scene.add(coin);
+}
+
+// --- Score ---
+let score = 0;
+const scoreElement = document.createElement("div");
+scoreElement.style.position = "absolute";
+scoreElement.style.top = "20px";
+scoreElement.style.left = "20px";
+scoreElement.style.fontSize = "24px";
+scoreElement.style.color = "white";
+scoreElement.innerHTML = `Score: ${score}`;
+document.body.appendChild(scoreElement);
 
 const clock = new THREE.Clock();
 
@@ -33,16 +57,36 @@ function animate() {
   leftWall.material.map.offset.y += 0.001;
   rightWall.material.map.offset.y += 0.001;
 
+  const halfRoad = roadWidth / 2;
+  if (robot.position.x < -halfRoad + 0.5) robot.position.x = -halfRoad + 0.5;
+  if (robot.position.x > halfRoad - 0.5) robot.position.x = halfRoad - 0.5;
+
+  
+  robot.velocity.x = Keys.left.pressed ? -0.1 : Keys.right.pressed ? 0.1 : 0;
+  robot.position.x += robot.velocity.x;
+  robot.position.z -= 0.1;
+
+
+  coins.forEach((coin, index) => {
+    coin.update();
+
+    const robotBox = new THREE.Box3().setFromObject(robot);
+    if (robotBox.intersectsBox(coin.boundingBox)) {
+      scene.remove(coin);
+      coins.splice(index, 1);
+      score += 10;
+      scoreElement.innerHTML = `Score: ${score}`;
+    }
+  });
+
+
   camera.position.set(
     robot.position.x,
-    robot.position.y + 2,
+    robot.position.y + 3,
     robot.position.z + 5
   );
   camera.lookAt(robot.position);
 
-  robot.velocity.x = Keys.left.pressed ? -0.1 : Keys.right.pressed ? 0.1 : 0;
-  robot.position.x += robot.velocity.x;
-  robot.position.z -= 0.1;
 
   renderer.render(scene, camera);
 }
