01b205b29c031780c99e020dd47c115020cbb184
Aswin Krishna <aswinkrishna005@gmail.com>
2025-08-30 10:17:34 +0530

    avatar changed

--- src/objects/animatedRobot.js
+++ src/objects/animatedRobot.js
@@ -3,7 +3,7 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 export function loadAnimatedRobot(scene, onLoaded) {
   const loader = new GLTFLoader();
-  loader.load('/runBoy.glb', (gltf) => {
+  loader.load('/avatar.glb', (gltf) => {
     const model = gltf.scene;
 
     model.scale.set(1, 1, 1);
